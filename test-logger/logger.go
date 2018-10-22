657b16b58baace61cc5a793b8857543252f27a5c
Jocelyn Schulz <jocelyn.schulz@gmail.com>
2018-10-22 13:38:20 -0400

    Add flags needed by Go.

--- test-logger/logger.go
+++ test-logger/logger.go
@@ -6,15 +6,29 @@ package main
 import (
 	"fmt"
 	"os"
+	"flag"
 
 	"github.com/golang/glog"
 )
 
+func usage() {
+	fmt.Fprintf(os.Stderr, "usage: example -stderrthreshold=[INFO|WARN|FATAL] -log_dir=[string]\n", )
+	flag.PrintDefaults()
+	os.Exit(2)
+}
+
+func init() {
+	flag.Usage = usage
+	// NOTE: This next line is key you have to call flag.Parse() for the command line
+	// options or "flags" that are defined in the glog module to be picked up.
+	flag.Parse()
+}
+
 func main() {
-	glog.Info("!!!!AN INFO MESSAGE>!?")
-	glog.Info("!!!!ANOTHER INFO MESSAGE>!?")
-	glog.Error("!!!!This is an ERROR message!@$ hurrah")
-	glog.V(2).Infoln("!!! this is a v2 info line ok")
-	glog.Warning("!!!!WARNWARN MESSAGE>!?")
+	glog.Info("!an info message")
+	glog.Info("@#another info")
+	glog.Error("weewoo errah")
+	glog.V(2).Infoln("&this is a v2 info line ok")
+	glog.Warning("^WARN^")
 	glog.Flush()
 }
